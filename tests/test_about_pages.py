2036642959be40f2c074b211522d62aeab6e6839
kaw393939 <keith@webizly.com>
2023-01-01 21:13:35 -0500

    test if it fails

--- tests/test_about_pages.py
+++ tests/test_about_pages.py
@@ -5,5 +5,5 @@ from application.database import User, db
 
 def test_about_route(client):
     response = client.get("/about")
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert b"About" in response.data
